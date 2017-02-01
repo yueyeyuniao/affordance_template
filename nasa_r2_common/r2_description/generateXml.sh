6c4d2ac9b59285bb49072bff050dd353a7487269
PengChang <yueyeyuniao>
2017-01-31 21:33:49 -0500

    remove r2 start with valkyrie

--- nasa_r2_common/r2_description/generateXml.sh
+++
@@ -1,8 +0,0 @@
-for file in `rospack find r2_description`/robots/*.xacro
-do
-    filename=${file%.*o}
-    echo $file
-    rosrun xacro xacro.py $file > "$filename.xml"
-    mv "$filename.xml" `rospack find r2_description`/robots
-done
-exit 0
--- nasa_r2_simulator/r2_gazebo/generateXml.sh
+++
@@ -1,8 +0,0 @@
-for file in `rospack find r2_gazebo`/robots/*.xacro
-do
-    filename=${file%.*o}
-    echo $file
-    rosrun xacro xacro.py $file > "$filename.xml"
-    mv "$filename.xml" `rospack find r2_gazebo`/robots
-done
-exit 0
